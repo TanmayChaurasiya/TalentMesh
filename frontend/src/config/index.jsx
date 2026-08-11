3a27cf779f8565fa1b7f38b21723909b540ef117
TanmayChaurasiya <mrbindastanmay@gmail.com>
2026-08-11 20:53:59 +0100

    first commit

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL="http://localhost:3002";
+export const BASE_URL="https://talentmesh-l4hd.onrender.com";
 export const clientServer=axios.create({
     baseURL:BASE_URL
 })
